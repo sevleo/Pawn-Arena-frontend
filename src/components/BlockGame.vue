aea1dabdf1d571cab8e5965ecc8c46057a8093ae
Seva <seva.leonov@hotmail.com>
2024-08-06 22:14:58 +0700

    Update

--- src/components/BlockGame.vue
+++ src/components/BlockGame.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from 'vue'
-import { setup } from '@/utilities/setup'
-import { updateDirections, updateBoost, fireBullet } from '../services/webSocket'
+import { ref, onMounted, onUnmounted, type Ref } from 'vue'
+import { type allPawns } from '@/types/allPawns'
 
 // List of active directions
 const activeDirections = ref<Set<string>>(new Set())
@@ -11,8 +10,6 @@ let animationFrameId: number | null = null
 
 // CanvasRef for canvas drawing
 const canvasRef = ref<HTMLCanvasElement | null>(null)
-
-const health = ref<number>(0)
 
 // Frame update loop
 const animateMovement = () => {
@@ -37,15 +34,6 @@ const handleKeyDown = (event: KeyboardEvent) => {
       updateDirections(activeDirections)
     }
   }
-
-  // Start boosting
-  if (event.key === 'Shift') {
-    updateBoost(true)
-  }
-
-  if (event.key === ' ') {
-    fireBullet()
-  }
 }
 
 const handleKeyUp = (event: KeyboardEvent) => {
@@ -67,15 +55,10 @@ const handleKeyUp = (event: KeyboardEvent) => {
       updateDirections(activeDirections)
     }
   }
-
-  // Stop boosting
-  if (event.key === 'Shift') {
-    updateBoost(false)
-  }
 }
 
 onMounted(() => {
-  setup(canvasRef, health)
+  setup(canvasRef)
   window.addEventListener('keydown', handleKeyDown)
   window.addEventListener('keyup', handleKeyUp)
   animateMovement()
@@ -88,14 +71,63 @@ onUnmounted(() => {
     cancelAnimationFrame(animationFrameId)
   }
 })
+
+let context: CanvasRenderingContext2D | null = null
+let ws: WebSocket
+
+function setup(canvasRef: Ref<HTMLCanvasElement | null>) {
+  initializeCanvas(canvasRef)
+  setupWebSocket(canvasRef)
+}
+
+function initializeCanvas(canvasRef: Ref<HTMLCanvasElement | null>) {
+  if (canvasRef.value) {
+    context = canvasRef.value.getContext('2d')
+  }
+}
+
+const setupWebSocket = (canvasRef: Ref<HTMLCanvasElement | null>) => {
+  ws = new WebSocket(`${import.meta.env.VITE_BACKEND_URL}`)
+
+  ws.onmessage = (event) => {
+    const msg = JSON.parse(event.data)
+
+    if (msg.type === 'gameState') {
+      console.log(msg)
+      drawPositions(canvasRef, msg.data.allPawns)
+    }
+  }
+
+  ws.onclose = () => {
+    console.log('WebSocket connection closed')
+  }
+}
+
+function drawPositions(canvasRef: Ref<HTMLCanvasElement | null>, allPawns: allPawns) {
+  if (context && canvasRef.value) {
+    context.clearRect(0, 0, canvasRef.value.width, canvasRef.value.height)
+    context.fillStyle = '#333300' // Set your desired background color here
+    context.fillRect(0, 0, canvasRef.value.width, canvasRef.value.height)
+    // Draw Controllable Units
+    Object.entries(allPawns).forEach(([, value]) => {
+      if (context) {
+        context.fillStyle = 'white'
+        context.beginPath()
+        context.arc(value.position.x, value.position.y, value.radius, 0, Math.PI * 2) // Draw a circle with radius 10
+        context.fill()
+      }
+    })
+  }
+}
+
+function updateDirections(activeDirections: Ref<Set<string>>) {
+  ws.send(JSON.stringify({ type: 'move', data: Array.from(activeDirections.value) }))
+}
 </script>
 
 <template>
   <div>
     <canvas ref="canvasRef" width="800" height="800" style="border: 0.5px solid wheat"> </canvas>
-    <div>
-      <p>Health: {{ health }}</p>
-    </div>
   </div>
 </template>
 
--- src/utilities/setup.ts
+++
@@ -1,8 +0,0 @@
-import { type Ref } from 'vue'
-import { setupWebSocket } from '../services/webSocket'
-import { initializeCanvas } from './canvasManager'
-
-export function setup(canvasRef: Ref<HTMLCanvasElement | null>, health: Ref<number>) {
-  initializeCanvas(canvasRef)
-  setupWebSocket(canvasRef, health)
-}
