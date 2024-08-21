3a194e387eb789c839c57f6802256af3fb57b200
Seva <seva.leonov@hotmail.com>
2024-08-21 22:16:51 +0700

    Update

--- src/config/gameConstants.ts
+++ src/config/gameConstants.ts
@@ -6,6 +6,6 @@ const MOVEMENT_SPEED = 2
 const GAME_SPEED_RATE = 16.67
 
 // Network
-const INTERPOLATION_OFFSET = 500 // Should be equal to frequency of websocket messages from server
+const INTERPOLATION_OFFSET = 50 // Should be equal to frequency of websocket messages from server
 
 export { RADIUS, MOVEMENT_SPEED, GAME_SPEED_RATE, INTERPOLATION_OFFSET }
