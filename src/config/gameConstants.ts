e8c7136e6f243dfdfce25f0653aecf06efa294ef
Seva <seva.leonov@hotmail.com>
2024-08-23 14:59:04 +0700

    Update offset interpol

--- src/config/gameConstants.ts
+++ src/config/gameConstants.ts
@@ -6,6 +6,6 @@ const MOVEMENT_SPEED = 100
 const GAME_SPEED_RATE = 16.67
 
 // Network
-const INTERPOLATION_OFFSET = 500 // Should be close to BROADCAST_RATE_INTERVAL on server for better performance
+const INTERPOLATION_OFFSET = 200 // Should be close to BROADCAST_RATE_INTERVAL on server for better performance
 
 export { RADIUS, MOVEMENT_SPEED, GAME_SPEED_RATE, INTERPOLATION_OFFSET }
