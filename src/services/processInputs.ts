8ad3b650118a94929d38c2f7c7799395b6ead709
Seva <seva.leonov@hotmail.com>
2024-08-23 18:17:46 +0700

    Update

--- src/services/processInputs.ts
+++ src/services/processInputs.ts
@@ -1,5 +1,6 @@
 import { gameState } from './gameState'
 import { type Input } from '@/types/Input'
+import { Body } from 'matter-js'
 
 // Get inputs and send them to the server
 function processInputs() {
@@ -36,7 +37,26 @@ function processInputs() {
 
   // Do client-side prediction.
   if (gameState.entity_id !== null) {
-    gameState.entities[gameState.entity_id]?.applyInput(input)
+    const entity = gameState.entities[gameState.entity_id]
+    // entity.applyInput(input)
+
+    const xVelocity = input.active_keys.right
+      ? entity.speed
+      : input.active_keys.left
+        ? -entity.speed
+        : 0
+    const yVelocity = input.active_keys.up
+      ? -entity.speed
+      : input.active_keys.down
+        ? entity.speed
+        : 0
+
+    // Predict new position based on velocity and delta time
+    entity.position.x += xVelocity * dt_sec
+    entity.position.y += yVelocity * dt_sec
+
+    // Apply predicted position
+    Body.setPosition(entity.entityBody, entity.position)
   }
 
   // Save this input for later reconciliation.
